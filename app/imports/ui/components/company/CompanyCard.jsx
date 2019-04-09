f33e79a01dae1b43fa94e7ea7f6c832c43d00d10
j-taki <46728308+j-taki@users.noreply.github.com>
2019-04-09 11:32:33 -1000

    Company Index Page (#19)
    closes: #3 
    
    added template for jobs show page as well.

---
+++ app/imports/ui/components/company/CompanyCard.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Card, Icon, Image, List } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+
+const imageStyle = {
+  width: '100%',
+};
+
+export default function CompanyCard(props) {
+
+  const { companyName, address, website, _id } = props.company;
+  return (
+
+      <Card raised>
+        <Image src='https://via.placeholder.com/150' style={imageStyle} centered></Image>
+        <Card.Content>
+          <Card.Header>{companyName}</Card.Header>
+          <Card.Meta>
+            <span className='date'>{address}</span>
+          </Card.Meta>
+          {/* <Card.Description>{summary.substring(0, summaryMaxLen) + '...'}</Card.Description> */}
+          <List>
+            <Link to={`/companies/${_id}`}>
+              <List.Item>{`Positions Open: 3`}</List.Item>
+            </Link>
+          </List>
+        </Card.Content>
+        <Card.Content extra>
+            <Icon name='globe' />
+            {website}
+        </Card.Content>
+      </Card>       
+  );
+}
+
+CompanyCard.propTypes = {
+  company: PropTypes.object.isRequired,
+  match: PropTypes.string,
+};
---
+++ app/imports/ui/pages/ShowCompany.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Meteor } from 'meteor/meteor';
+import { Loader } from 'semantic-ui-react';
+import { withTracker } from 'meteor/react-meteor-data';
+import { Companies } from '../../api/company/company';
+
+class ShowCompany extends Component {
+  renderPage() {
+    const { companyName } = this.props.company;
+
+    return (
+      <div>
+        <h1>{companyName}</h1>
+      </div>
+    );
+  }
+
+  render() {
+    return this.props.ready
+      ? this.renderPage()
+      : <Loader active>Getting data</Loader>;
+  }
+}
+
+ShowCompany.propTypes = {
+  company: PropTypes.object,
+  ready: PropTypes.bool,
+};
+
+export default withTracker(({ match }) => {
+  const documentId = match.params.companyId;
+
+  const subscription = Meteor.subscribe('Companies');
+  return {
+    company: Companies.findOne({ _id: documentId }),
+    ready: subscription.ready(),
+  };
+})(ShowCompany);
