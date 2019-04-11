9960fef25e68127a57ee188025bc3418f977b5b8
Josh Walters <39789483+joshnwalters@users.noreply.github.com>
2019-04-10 16:33:33 -1000

    Intermediate push

--- app/imports/ui/pages/ShowCompany.jsx
+++ app/imports/ui/pages/ShowCompany.jsx
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Meteor } from 'meteor/meteor';
-import { Loader } from 'semantic-ui-react';
+import { Loader, Container } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import { Companies } from '../../api/company/company';
+import CompanyComponent from '/imports/ui/components/CompanyComponent';
 
 class ShowCompany extends Component {
-  renderPage() {
-    const { companyName } = this.props.company;
-
-    return (
-      <div>
-        <h1>{companyName}</h1>
-      </div>
-    );
-  }
 
   render() {
     return this.props.ready
-      ? this.renderPage()
-      : <Loader active>Getting data</Loader>;
+        ? this.renderPage()
+        : <Loader active>Getting data</Loader>;
+  }
+
+    renderPage() {
+      return (
+          <Container>
+            {this.props.company.map((company) => <CompanyComponent key={company._id} company={company} />)};
+          </Container>
+      );
   }
 }
 
