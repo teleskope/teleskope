8143e5393d68c7770fd47b6b42240a1ea32f5c5c
Matthew Kim <45888473+mattkim009@users.noreply.github.com>
2019-04-10 17:06:50 -1000

    Issue 10 landing page (#24)
    * background image commit
    
    * background-image-2-commit
    
    * background-image-3
    
    * background-image-4
    
    * final commit

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,24 +1,68 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Icon, Header, Button, Segment, Image } from 'semantic-ui-react';
+import { Meteor } from 'meteor/meteor';
+import { withTracker } from 'meteor/react-meteor-data';
+import { withRouter, NavLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
+//import logo from '../../../public/images/TeleSKOPE.png';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-        <Grid verticalAlign='middle' textAlign='center' container>
 
-          <Grid.Column width={4}>
-            <Image size='small' circular src="/images/meteor-logo.png"/>
-          </Grid.Column>
+      <div >
+        <Segment id="teleskope-background" attached="top" inverted padded="very" size="large" textAlign="center">
+          <Header as="h1" size="huge">TeleSkope</Header>
+            {/*<Image src="images/TeleSKOPE.png" size='medium' />*/}
+          <Header as="h2" size="medium">Helping employers find stars</Header>
+          <Header as="h3">New?  Choose a role below and register!</Header>
+          <Button as={NavLink} exact to="/signup" key='StudentSignup'>Student</Button>
+          <Button as={NavLink}  exact to="/signup" key='CompanySignup'>Company</Button>
+          {this.props.currentUser ? (
+            [<Button as={NavLink} activeClassName="active" exact to="/add" key='add'>Add Stuff</Button>,
+              <Button as={NavLink} activeClassName="active" exact to="/list" key='list'>List Stuff</Button>,
+              <Button as={NavLink} activeClassName="active" exact to="/test" key='test'>Textbook</Button>]
 
-          <Grid.Column width={8}>
-            <h1>Welcome to this template</h1>
-            <p>Now get to work and modify this app!</p>
-          </Grid.Column>
+          ) : ''}
 
-        </Grid>
+        </Segment>
+
+        <Segment padded="very" basic>
+          <Grid container centered stackable columns={3}>
+
+            <Grid.Column textAlign='center'>
+              <Icon size='huge' name="star outline" />
+              <Header as="h1" >1,000</Header>
+              <Header as="h3" >Companies looking for stars</Header>
+            </Grid.Column>
+
+            <Grid.Column textAlign='center'>
+              <Icon size='huge' name="connectdevelop" />
+              <Header as="h1" >2,000</Header>
+              <Header as="h3" >connections made</Header>
+            </Grid.Column>
+
+            <Grid.Column textAlign='center'>
+              <Icon size='huge' name="money bill alternate outline" />
+              <Header as="h1" >1,210</Header>
+              <Header as="h3" >Career opportunities available</Header>
+
+            </Grid.Column>
+
+          </Grid>
+        </Segment>
+      </div>
     );
   }
 }
 
-export default Landing;
+Landing.propTypes = {
+  currentUser: PropTypes.string,
+};
+
+const LandingContainer = withTracker(() => ({
+  currentUser: Meteor.user() ? Meteor.user().username : '',
+}))(Landing);
+
+export default withRouter(LandingContainer);
