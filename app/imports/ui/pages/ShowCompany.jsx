77fd25b3d0e6b4958171dbf724da30973ad8699d
Josh Walters <39789483+joshnwalters@users.noreply.github.com>
2019-04-10 17:53:52 -1000

    Potentially complete

--- app/imports/ui/pages/ShowCompany.jsx
+++ app/imports/ui/pages/ShowCompany.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Meteor } from 'meteor/meteor';
-import { Loader, Container } from 'semantic-ui-react';
+import { Loader, Container, Image, Header, Icon, Grid, Segment, Menu } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import { Companies } from '../../api/company/company';
-import CompanyComponent from '/imports/ui/components/CompanyComponent';
+
 
 class ShowCompany extends Component {
 
@@ -16,9 +16,40 @@ class ShowCompany extends Component {
 
     renderPage() {
       return (
-          <Container>
-            {this.props.company.map((company) => <CompanyComponent key={company._id} company={company} />)};
-          </Container>
+          <Grid>
+            <Grid.Row columns={2}>
+              <Grid.Column>
+                <Image src={this.props.company.image} size='huge'/>
+              </Grid.Column>
+              <Grid.Column>
+                <Header as='h1'>{this.props.company.companyName}</Header>
+                <Header as='h3'><Icon className="map marker alternate icon"/>
+                  {this.props.company.address}
+                </Header>
+                <Container>
+                  <Menu borderless text>
+                  <Menu.Item><Icon size='large' className="twitter icon"/></Menu.Item>
+                  <Menu.Item><Icon size='large' className="linkedin icon"/></Menu.Item>
+                  <Menu.Item><Icon size='large' className="github icon"/></Menu.Item>
+                  <Menu.Item><Icon size='large' className="envelope outline icon"/></Menu.Item>
+                  </Menu>
+                </Container>
+              </Grid.Column>
+            </Grid.Row>
+            <Segment></Segment>
+            <Grid.Row>
+              <Container text>
+              <Header as='h2'>Company Description</Header>
+                {this.props.company.summary}
+              </Container>
+            </Grid.Row>
+            <Segment></Segment>
+            <Grid.Row>
+              <Container text>
+                <Header as='h2'>Current Openings</Header>
+              </Container>
+            </Grid.Row>
+          </Grid>
       );
   }
 }
