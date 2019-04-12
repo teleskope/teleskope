701c7fd401eb653d670e39aeac39c8de91fdb74d
Jeff <jtaki@hawaii.edu>
2019-04-12 11:42:45 -1000

    separated company schema from user

--- app/imports/api/company/company.js
+++ app/imports/api/company/company.js
@@ -1,22 +1,18 @@
 import { Mongo } from 'meteor/mongo';
 import SimpleSchema from 'simpl-schema';
 import { Tracker } from 'meteor/tracker';
-import { UserSchema } from '/imports/api/user/user';
 
-/** Create a Meteor collection. */
 const Companies = new Mongo.Collection('Companies');
 
-/** Create a schema to constrain the structure of documents associated with this collection. */
 const CompanySchema = new SimpleSchema({
-  companyName: { type: String, required: true },
+  name: { type: String, required: true },
   address: { type: String, required: true },
   zipCode: { type: String, required: true },
   website: String,
   summary: String,
-}, { tracker: Tracker });
+  owner: Array,
+}, { tracker: Tracker, requiredByDefault: false });
 
-CompanySchema.extend(UserSchema);
 Companies.attachSchema(CompanySchema);
 
-/** Make the collection and schema available to other code. */
 export { Companies, CompanySchema };
