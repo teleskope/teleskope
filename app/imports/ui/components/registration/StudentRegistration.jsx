2360951b845f7ea627ff86844976f33e9ed022de
Matthew Kim <45888473+mattkim009@users.noreply.github.com>
2019-04-18 11:47:27 -1000

    fixed name forms to be fluid

--- app/imports/ui/components/registration/StudentRegistration.jsx
+++ app/imports/ui/components/registration/StudentRegistration.jsx
@@ -49,14 +49,14 @@ export default class StudentRegistration extends Component {
                 onChange={this.handleChange}
             />
             <Form.Group widths='equal'>
-              <Form.Input
+              <Form.Input fluid
                   label="First Name"
                   name="firstName"
                   type="text"
                   placeholder="First Name"
                   onChange={this.handleChange}
               />
-              <Form.Input
+              <Form.Input fluid
                   label="Last Name"
                   name="lastName"
                   type="text"
