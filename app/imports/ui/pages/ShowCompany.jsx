3c5fa974c039f88a01b5f62f5507311b88e9e16e
j-taki <46728308+j-taki@users.noreply.github.com>
2019-04-25 22:21:46 -1000

    Modify fixtures (#62)
    * fixed skills array in job
    
    fixed job card and show job
    
    * updated schemas, removed job schema
    
    * updated schemas, contains errors
    
    * changed owner to owneres,
    
    * changed owner to owners
    
    * fixed skille
    
    * fixed skills in JSON file
    
    * added skills to jobs
    
    * created JSON files for fixtures
    
    * fixed the JSON
    
    * updated schemas, removed job schema
    
    * updated schemas, contains errors
    
    * fixed skills in JSON file
    
    * created JSON files for fixtures
    
    * fixed the JSON
    
    * updated showcompany
    
    * fixed json data fixtures

--- app/imports/ui/pages/ShowCompany.jsx
+++ app/imports/ui/pages/ShowCompany.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Meteor } from 'meteor/meteor';
-import { Loader, Container, Image, Header, Icon, Grid, Menu, Card } from 'semantic-ui-react';
+import { Loader, Container, Header, Icon, Grid, Menu, Card, Button } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import { Companies } from '../../api/company/company';
+import { Profiles } from '../../api/profile/profile';
 import JobCard from '../components/job/JobCard';
 
 class ShowCompany extends Component {
@@ -11,56 +12,58 @@ class ShowCompany extends Component {
     return this.props.ready ? this.renderPage() : <Loader active>Getting data</Loader>;
   }
 
-    renderPage() {
-      const { jobs } = this.props.company;
-      return (
-          <Grid style={{ marginTop: '2em' }}>
-            <Grid.Row columns={2}>
-              <Grid.Column>
-                <Image src={this.props.company.image} size='huge'/>
-              </Grid.Column>
-              <Grid.Column>
+  renderPage() {
+    const { jobs } = this.props.company;
+    return (
+        <Grid style={{ marginTop: '2em' }}>
+          <Grid.Row columns={2}>
+            <Grid.Column>
+            </Grid.Column>
+            <Grid.Column>
+              <Grid.Row>
                 <Header as='h1'>{this.props.company.name}</Header>
-                <Header as='h3'><Icon className="map marker alternate icon"/>
-                  {this.props.company.address}
-                </Header>
-                <Container>
-                  <Menu borderless text>
+              </Grid.Row>
+              <Header as='h3'><Icon className="map marker alternate icon"/>
+                {this.props.company.address}
+              </Header>
+              <Container>
+                <Menu borderless text>
                   <Menu.Item><Icon size='large' className="twitter icon"/></Menu.Item>
                   <Menu.Item><Icon size='large' className="linkedin icon"/></Menu.Item>
                   <Menu.Item><Icon size='large' className="github icon"/></Menu.Item>
                   <Menu.Item><Icon size='large' className="envelope outline icon"/></Menu.Item>
-                  </Menu>
-                </Container>
-              </Grid.Column>
-            </Grid.Row>
-            <Grid.Row>
-              <Container text>
+                </Menu>
+              </Container>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Container text>
               <Header as='h2'>Company Description</Header>
-                {this.props.company.summary}
-              </Container>
-            </Grid.Row>
-            <Grid.Row>
-              <Container text>
-                <Header as='h2'>Current Openings</Header>
-              </Container>
-            </Grid.Row>
-            <Grid.Row>
-              <Container>
+              {this.props.company.summary}
+            </Container>
+          </Grid.Row>
+          <Grid.Row>
+            <Container text>
+              <Header as='h2'>Current Openings</Header>
+            </Container>
+          </Grid.Row>
+          <Grid.Row>
+            <Container>
               <Card.Group stackable>
                 {jobs.map((job, index) => (
                     <JobCard key={index} job={job} />
                 ))}
               </Card.Group>
-              </Container>
-            </Grid.Row>
-          </Grid>
-      );
+            </Container>
+          </Grid.Row>
+        </Grid>
+    );
   }
 }
 
 ShowCompany.propTypes = {
   company: PropTypes.object,
+  profile: PropTypes.object,
   ready: PropTypes.bool,
 };
 
@@ -68,8 +71,10 @@ export default withTracker(({ match }) => {
   const documentId = match.params.companyId;
 
   const subscription = Meteor.subscribe('Companies');
+  const subscription2 = Meteor.subscribe('Profiles');
   return {
     company: Companies.findOne({ _id: documentId }),
-    ready: subscription.ready(),
+    profile: Profiles.findOne({ _id: Meteor.userId() }),
+    ready: (subscription.ready() && subscription2.ready()),
   };
 })(ShowCompany);
