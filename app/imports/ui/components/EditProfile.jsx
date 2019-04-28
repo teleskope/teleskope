ac398e0df952eded425fe7e0564f758fc27a8d00
darius-despain <darius.despain@gmail.com>
2019-04-27 14:14:27 -1000

    Still not working
    Need to fix submit, required fields are preventing submit.

--- app/imports/ui/components/EditProfile.jsx
+++ app/imports/ui/components/EditProfile.jsx
@@ -61,7 +61,6 @@ export default function EditProfile(props){
         </Modal>
     );
  // }
-
 }
 
 /** Require the presence of a Stuff document in the props object. Uniforms adds 'model' to the props, which we use. */
