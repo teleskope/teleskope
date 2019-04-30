64a0815ab31f461f4ad842adad08f1f9b1e288fc
Josh Walters <39789483+joshnwalters@users.noreply.github.com>
2019-04-30 10:57:24 -1000

    changed css on show company to the css from the css file

--- app/imports/ui/pages/ShowCompany.jsx
+++ app/imports/ui/pages/ShowCompany.jsx
@@ -81,7 +81,7 @@ class ShowCompany extends Component {
 
       return null;
     }
-    return <Modal style={modal} trigger={<Button
+    return <Modal id='modal' trigger={<Button
         content='Edit'
         color='black'
         floated='right'
@@ -90,7 +90,7 @@ class ShowCompany extends Component {
       <Grid container centered>
         <Grid.Column>
           <Header as="h2" textAlign="center">Edit Company</Header>
-          <AutoForm schema={CompanySchema} onSubmit={this.submit} model={this.props.doc}>
+          <AutoForm schema={CompanySchema} onSubmit={this.submit} model={this.props.company}>
             <Segment>
               <TextField name='name'/>
               <TextField name='owners'/>
