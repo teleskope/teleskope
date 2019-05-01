87bbc2b15e1afe58952a7889a23ecdc700a75ca5
darius-despain <darius.despain@gmail.com>
2019-04-30 19:19:33 -1000

    Finished version 1 with navBar fixes
    Added My Profile button to right side of navBar. Finishes #35

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -66,6 +66,8 @@ class NavBar extends React.Component {
           ) : (
             <Dropdown text={this.props.currentUser} pointing="top right" icon={'user'}>
               <Dropdown.Menu>
+                <Dropdown.Item icon="user" text="My Profile" as={NavLink} exact to="/profile"/>
+
                 <Dropdown.Item icon="sign out" text="Sign Out" as={NavLink} exact to="/"
                                onClick={(() => Meteor.logout())}
                 />
--- app/imports/ui/pages/Dashboard.jsx
+++ app/imports/ui/pages/Dashboard.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
-import { Grid, Icon, Header, Button, Segment, Image, Container, Advertisement } from 'semantic-ui-react';
+import { Grid, Header, Segment, Image, Container, Advertisement, List } from 'semantic-ui-react';
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
-import { withRouter, Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
+
+const profileStyle = {
+  padding: '50px 0px',
+};
+
+const segmentStyle = {
+  backgroundColor: '#455880',
+};
 
 class Dashboard extends React.Component {
   render() {
@@ -12,18 +20,43 @@ class Dashboard extends React.Component {
       <div>
         <Container>
           {/* Image here needs to be grabbed from the user's profile */}
-          <Image src='http://clipart-library.com/images/6cpop79oi.png' size='medium' circular />
-          <Grid columns='three'>
-            <Grid.Column stretched>
-              <Header as='h3' centered>Companies</Header>
-            </Grid.Column >
-            <Grid.Column stretched>
-              <Header as='h3' centered>Notifications</Header>
-
-            </Grid.Column>
-            <Grid.Column stretched centered>
-              <Advertisement unit='vertical rectangle' test='Advertisements' />
-            </Grid.Column>
+          <Grid columns='equal'>
+            <Grid.Row>
+              <Grid.Column>
+                <Image src='http://clipart-library.com/images/6cpop79oi.png' size='medium' style={profileStyle}/>
+              </Grid.Column>
+              <Grid.Column style={profileStyle}>
+                <Header as='h2'> Welcome First Last </Header>
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column stretched>
+                <Segment tertiary inverted compact style={segmentStyle}>
+                  <Header as='h3' textalign='center'> Companies You Are Following </Header>
+                  <List>
+                    <List.Item>Company A</List.Item>
+                    <List.Item>Company B</List.Item>
+                    <List.Item>Company D</List.Item>
+                  </List>
+                </Segment>
+              </Grid.Column>
+              <Grid.Column stretched>
+                <Segment secondary inverted style={segmentStyle}>
+                  <Header as='h3'> Notifications </Header>
+                  <Segment.Group tertiary>
+                    <Segment tertiary>Company A is interested in you!</Segment>
+                    <Segment tertiary>Company B is interested in you!</Segment>
+                    <Segment tertiary>Company C is interested in you!</Segment>
+                    <Segment tertiary>Company A is interested in you!</Segment>
+                    <Segment tertiary>Company B is interested in you!</Segment>
+                  </Segment.Group>
+                </Segment>
+              </Grid.Column>
+              <Grid.Column stretched>
+                <Advertisement unit='vertical rectangle' test='Advertisements' long centered/>
+                <Advertisement unit='vertical rectangle' test='Advertisements' long centered/>
+              </Grid.Column>
+            </Grid.Row>
           </Grid>
         </Container>
       </div>
