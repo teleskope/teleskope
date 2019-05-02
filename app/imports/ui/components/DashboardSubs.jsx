9789762b8b437c9825add19e62644908a7733750
Jeff <jtaki@hawaii.edu>
2019-05-01 19:48:25 -1000

    added label for company count on dash sidebar

--- app/imports/ui/components/DashboardSubs.jsx
+++ app/imports/ui/components/DashboardSubs.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Menu } from 'semantic-ui-react';
+import { Menu, Label } from 'semantic-ui-react';
 
 export default class DashboardSubs extends Component {
   constructor() {
@@ -25,41 +25,17 @@ export default class DashboardSubs extends Component {
     });
   }
 
-  myFollowing() {
-    const { activeItem } = this.state;
-    return _.map(this.props.profile.following, (company) => {
-            return <Menu.Item
-                      name={company}
-                      active={activeItem === company}
-                      onClick={this.handleItemClick}
-                      key={company}
-                      // link href={`/#/companies/${company._id}`}
-                      >
-                      {company}
-                   </Menu.Item>;
-    });
-  }
-
   render() {
-    return this.props.profile.role === 'company' ? this.renderCompany() : this.renderStudent();
-  }
-
-  renderCompany() {
     const myCompanies = this.myCompanies();
 
     return (
       <Menu vertical fluid>
-        <Menu.Item><Menu.Header>My Companies</Menu.Header></Menu.Item>
-        { myCompanies }
-      </Menu>
-    );
-  }
-
-  renderStudent() {
-    const myCompanies = this.myCompanies();
-    return (
-      <Menu vertical fluid>
-        <Menu.Item><Menu.Header>Following</Menu.Header></Menu.Item>
+        <Menu.Item>
+          <Label color='teal'>{myCompanies.length}</Label>
+          <Menu.Header>
+            {this.props.profile.role === 'company' ? 'My Companies' : 'Following'}
+          </Menu.Header>
+        </Menu.Item>
         { myCompanies }
       </Menu>
     );
