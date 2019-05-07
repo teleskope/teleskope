8fafa6c0a199e6da8854d8d5bd702a9a2911be30
Josh Walters <39789483+joshnwalters@users.noreply.github.com>
2019-05-06 22:40:14 -1000

    changed from autoform to form

--- app/imports/ui/components/job/AddJob.jsx
+++ app/imports/ui/components/job/AddJob.jsx
@@ -1,15 +1,5 @@
 import React from 'react';
-import { CompanySchema } from '/imports/api/company/company';
-import PropTypes from 'prop-types';
-import { Grid, Segment, Header, Button, Modal } from 'semantic-ui-react';
-import AutoForm from 'uniforms-semantic/AutoForm';
-import TextField from 'uniforms-semantic/TextField';
-import LongTextField from 'uniforms-semantic/LongTextField';
-import SelectField from 'uniforms-semantic/SelectField';
-import SubmitField from 'uniforms-semantic/SubmitField';
-import HiddenField from 'uniforms-semantic/HiddenField';
-import ErrorsField from 'uniforms-semantic/ErrorsField';
-import { Bert } from 'meteor/themeteorchef:bert';
+import { Grid, Segment, Header, Button, Modal, Message, Form } from 'semantic-ui-react';
 import { Meteor } from 'meteor/meteor';
 
 /** Renders the Page for adding a document. */
@@ -17,25 +7,15 @@ class AddJob extends React.Component {
 
   /** Bind 'this' so that a ref to the Form can be saved in formRef and communicated between render() and submit(). */
   constructor(props) {
-      super(props);
-      this.state = {
-        title: '',
-        employmentType: '',
-        requirements: '',
-        description: '',
-        skills: '',
-        error: '',
-      };
-   }
-
-  /** Notify the user of the results of the submit. If successful, clear the form. */
-  insertCallback(error) {
-    if (error) {
-      Bert.alert({ type: 'danger', message: `Add failed: ${error.message}` });
-    } else {
-      Bert.alert({ type: 'success', message: 'Add succeeded' });
-      this.formRef.reset();
-    }
+    super(props);
+    this.state = {
+      title: '',
+      employmentType: '',
+      requirements: '',
+      description: '',
+      skills: '',
+      error: '',
+    };
   }
 
 
@@ -50,24 +30,71 @@ class AddJob extends React.Component {
   render() {
     return (
         <Modal id='modal' trigger={<Button
-            content='Add Job'
+            content='Edit'
             color='green'
         />} closeIcon>
-        <Grid container centered>
-          <Grid.Column>
-            <Header as="h2" textAlign="center">Add Job</Header>
-            <AutoForm ref={(ref) => { this.formRef = ref; }} schema={CompanySchema} onSubmit={this.submit}>
-              <Segment>
-                <TextField name='jobs.$.title'/>
-                <SelectField name='jobs.$.employmentType'/>
-                <LongTextField name='jobs.$.requirements'/>
-                <LongTextField name='jobs.$.description'/>
-                <SubmitField value='Submit'/>
-                <ErrorsField/>
-              </Segment>
-            </AutoForm>
-          </Grid.Column>
-        </Grid>
+          <Grid container centered>
+            <Grid.Column>
+              <Header as="h2" textAlign="center">Add Jobs</Header>
+        <React.Fragment>
+          <Form onSubmit={this.handleSubmit}>
+            <Segment stacked>
+              <Form.Input
+                  label="Job Title"
+                  iconPosition="left"
+                  name="job.$.title"
+                  type="text"
+                  placeholder="Job Title"
+                  onChange={this.handleChange}
+                  required
+              />
+              <Form.Select
+                  label="Employment Type"
+                  iconPosition="left"
+                  name="jobs.$.employmentType"
+                  options={employmentType}
+                  placeholder="Employment Type"
+                  onChange={this.handleChange}
+                  required
+              />
+              <Form.Input fluid
+                          label="Requirements"
+                          name="job.$.requirements"
+                          type="text"
+                          placeholder="What the employee must have."
+                          onChange={this.handleChange}
+                          required
+              />
+              <Form.Input fluid
+                          label="Description"
+                          name="job.$.description"
+                          type="text"
+                          placeholder="A brief summary of the work that will be performed"
+                          onChange={this.handleChange}
+                          required
+              />
+              <Form.Input
+                  label="Skills"
+                  name="jobs.$.skills"
+                  type="text"
+                  placeholder="What skills would the employee need to be successful at this position"
+                  onChange={this.handleChange}
+              />
+
+              <Form.Button content="Submit" fluid primary/>
+            </Segment>
+          </Form>
+          {this.state.error === '' ? ('') :
+              (
+                  <Message
+                      error
+                      header="Add was not successful"
+                      content={this.state.error}
+                  />
+              )}
+        </React.Fragment>
+            </Grid.Column>
+          </Grid>
         </Modal>
     );
   }
